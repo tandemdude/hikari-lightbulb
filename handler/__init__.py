3de4de3059138bdfc4eace0c24f0e08799afaa7e
davfsa <davfsa@gmail.com>
2020-06-27 21:06:04 +0200

    Simplified use and tidied up a bit

--- handler/__init__.py
+++ handler/__init__.py
@@ -1,1 +1,1 @@
-from .handler import Handler
+from .command_handler import BotWithHandler as Bot
--- handler/command_handler.py
+++ handler/command_handler.py
@@ -1,17 +1,23 @@
 import typing
 import re
-from hikari.models import messages
 
-from .commands import Command
-from handler.handler import context
+from hikari import Bot
+from hikari.events import message
+from hikari.models import messages
+from hikari.events import base
+
+from handler import commands
+from handler import context
 
 ARGUMENT_REGEX = re.compile(r"(\".+\"|[^\s]+)")
 
 
-class Handler:
-    def __init__(self, prefix: str):
+class BotWithHandler(Bot):
+    def __init__(self, prefix: str, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.event_dispatcher.subscribe(message.MessageCreateEvent, self.handle)
         self.prefix = prefix
-        self.commands: typing.MutableMapping[str, Command] = {}
+        self.commands: typing.MutableMapping[str, commands.Command] = {}
 
     def command(self):
         commands = self.commands
@@ -19,15 +25,15 @@ class Handler:
         def decorate(func: typing.Callable):
             nonlocal commands
             if not commands.get(func.__name__):
-                commands[func.__name__] = Command.from_callable(func)
+                commands[func.__name__] = commands.Command.from_callable(func)
 
         return decorate
 
     def resolve_arguments(self, message: messages.Message) -> typing.List[str]:
         return ARGUMENT_REGEX.findall(message.content)
 
-    async def handle(self, message: messages.Message):
-        args = self.resolve_arguments(message)
+    async def handle(self, event: message.MessageCreateEvent) -> None:
+        args = self.resolve_arguments(event.message)
         # Check if the message was actually a command invocation
         if not args[0].startswith(self.prefix):
             return
@@ -37,5 +43,5 @@ class Handler:
             return
         invoked_command = self.commands[invoked_with]
 
-        command_context = context.Context(message, self.prefix, invoked_with, invoked_command)
+        command_context = context.Context(event.message, self.prefix, invoked_with, invoked_command)
         await invoked_command(command_context, *args[1:])

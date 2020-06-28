e596b4f1b3bd2adee92b92f35fb7fa2ead806dd4
tandemdude <olijdthompson@gmail.com>
2020-06-28 13:02:10 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+def long_description():
+    with open("README.md") as fp:
+        return fp.read()
+
+
+def parse_requirements_file(path):
+    with open(path) as fp:
+        dependencies = (d.strip() for d in fp.read().split("\n") if d.strip())
+        return [d for d in dependencies if not d.startswith("#")]
+
+
+setup(
+    name="handler",
+    version="0.0.1",
+    description="A simple to use command handler for Hikari",
+    long_description=long_description(),
+    long_description_content_type="text/markdown",
+    author="tandemdude",
+    author_email="tandemdude1@gmail.com",
+    url="https://gitlab.com/tandemdude/hikari-command-handler",
+    packages=["handler"],
+    install_requires=parse_requirements_file("requirements.txt"),
+    python_requires=">=3.8.0,<3.10",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+)

a2ce08f90f48f728afb95c6a5acfd80da9ad490d
tandemdude <olijdthompson@gmail.com>
2020-06-29 01:50:02 +0100

    Rebrand to lightbulb

--- lightbulb/stringview.py
+++ lightbulb/stringview.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
 # Copyright © Thomm.o 2020
 #
-# This file is part of Hikari Command Handler.
+# This file is part of Lightbulb.
 #
-# Hikari Command Handler is free software: you can redistribute it and/or modify
+# Lightbulb is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Hikari Command Handler is distributed in the hope that it will be useful,
+# Lightbulb is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with Hikari Command Handler. If not, see <https://www.gnu.org/licenses/>.
+# along with Lightbulb. If not, see <https://www.gnu.org/licenses/>.
 import typing
 
-from handler import errors
+from lightbulb import errors
 
 _quotes = {
     '"': '"',

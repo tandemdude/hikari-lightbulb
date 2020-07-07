abbb7fe0a9613b01bccd2facb7ee3b5dd103df95
tandemdude <olijdthompson@gmail.com>
2020-07-07 16:52:25 +0100

    Update release_webhook.py

--- release_webhook.py
+++ release_webhook.py
@@ -8,7 +8,7 @@ requests.post(
     json={
         "embeds": [{
             "title": f"Lightbulb v{lightbulb.__version__} released to PyPi.",
-            "description": f"Install with:\n```pip install -U hikari-lightbulb=={lightbulb.__version__}",
+            "description": f"Install with:\n```pip install -U hikari-lightbulb=={lightbulb.__version__}```",
         }]
     }
 )

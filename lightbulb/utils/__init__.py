cdb9c562671ce19179b70ccf18cf4773dbc4dcd4
thomm.o <olijdthompson@gmail.com>
2020-07-13 17:12:45 +0000

    Fixes #36.
    Issue related to ending command invocations with a `\`
    and it causing a resultant IndexError.

--- lightbulb/utils/__init__.py
+++ lightbulb/utils/__init__.py
@@ -15,4 +15,4 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with Lightbulb. If not, see <https://www.gnu.org/licenses/>.
-from lightbulb.utils import pag, nav
+from . import pag, nav

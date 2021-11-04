ce36cde0f3fee6f90536f71b2a79391c44e0b9e9
NeonJonn <jonathan.groult1@gmail.com>
2021-11-04 17:52:14 +0000

    Rewrite rest of the examples to work with v2

--- examples/extension_example.py
+++ examples/extension_example.py
@@ -15,4 +15,41 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with Lightbulb. If not, see <https://www.gnu.org/licenses/>.
-raise NotImplementedError
+# -*- coding: utf-8 -*-
+# Copyright © tandemdude 2020-present
+#
+# This file is part of Lightbulb.
+#
+# Lightbulb is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Lightbulb is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Lightbulb. If not, see <https://www.gnu.org/licenses/>.
+import lightbulb
+
+
+example_plugin = lightbulb.Plugin("ExamplePlugin")
+
+@example_plugin.command()
+@lightbulb.command("ping", "Checks that the bot is alive")
+@lightbulb.implements(lightbulb.commands.PrefixCommand, lightbulb.commands.SlashCommand)
+async def ping(self, ctx):
+    """Checks that the bot is alive"""
+    await ctx.respond("Pong!")
+
+
+
+def load(bot):
+    bot.add_plugin(example_plugin)
+
+
+def unload(bot):
+    bot.remove_plugin(example_plugin)
+

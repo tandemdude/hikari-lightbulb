6348abcd01f8420b8e76af25bc17c6f90c210ee6
Oliver Thompson <oliver@tryterra.co>
2022-01-17 18:38:57 +0000

    Update documentation and add build_invite_url

--- lightbulb/utils/__init__.py
+++ lightbulb/utils/__init__.py
@@ -15,6 +15,7 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with Lightbulb. If not, see <https://www.gnu.org/licenses/>.
+from __future__ import annotations
 
 __all__ = [
     "BaseParser",
@@ -36,7 +37,13 @@ __all__ = [
     "permissions_in",
     "prev_page",
     "stop",
+    "build_invite_url",
 ]
+
+import typing as t
+from urllib import parse
+
+import hikari
 
 from lightbulb.utils import data_store
 from lightbulb.utils import nav
@@ -50,3 +57,38 @@ from lightbulb.utils.pag import *
 from lightbulb.utils.parser import *
 from lightbulb.utils.permissions import *
 from lightbulb.utils.search import *
+
+if t.TYPE_CHECKING:
+    from lightbulb import app as app_
+
+
+def build_invite_url(
+    app: app_.BotApp,
+    permissions: hikari.Permissions = hikari.Permissions.NONE,
+    allow_commands: bool = False,
+) -> str:
+    """
+    Build an invite link for the given :obj:`~.app.BotApp` with the given permissions and scopes. This
+    adds the ``bot`` scope to the URL by default.
+
+    Args:
+        app (:obj:`~.app.BotApp`): :obj:`~.app.BotApp` instance to create the invite link for.
+        permissions (:obj:`hikari.Permissions`): Permissions to add to the invite link. Defaults to
+            :obj:`hikari.Permissions.NONE`.
+        allow_commands (:obj:`bool`): Whether to add the `applications.commands` scope to the invite link. Defaults
+            to ``False``.
+
+    Returns:
+        :obj:`str`: Created invite link.
+    """
+    if app.application is None:
+        raise ValueError("Cannot build invite url when 'app.application' is None")
+
+    raw_scopes = {"bot"}
+    if allow_commands:
+        raw_scopes.add("applications.commands")
+
+    scopes = " ".join(raw_scopes)
+    return "https://discord.com/api/oauth2/authorize?" + parse.urlencode(
+        {"client_id": app.application.id, "permissions": int(permissions), "scope": scopes}
+    )

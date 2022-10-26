c227963f5ddd4aa89841a656eb72c15d23f6cc17
PerchunPak <perchunpak@gmail.com>
2022-10-26 09:08:32 +0300

    Use `<` instead of `<=`
    Co-authored-by: davfsa <davfsa@gmail.com>

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
     zip_safe=False,
     install_requires=parse_requirements_file("requirements.txt"),
     extras_require={"crontrigger": parse_requirements_file("crontrigger_requirements.txt")},
-    python_requires=">=3.8.0,<=3.11",
+    python_requires=">=3.8.0,<3.12",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Framework :: AsyncIO",

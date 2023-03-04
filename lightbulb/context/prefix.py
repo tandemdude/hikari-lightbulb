15c3cdb4555f4d207347ef70efdb17dc1ff7d62c
davfsa <davfsa@gmail.com>
2023-03-04 13:57:52 +0100

    Fix deferral of slash sub-commands (#299)
    Co-authored-by: thomm.o <olijdthompson@gmail.com>

--- lightbulb/context/prefix.py
+++ lightbulb/context/prefix.py
@@ -66,6 +66,9 @@ class PrefixContext(base.Context):
         self._parser: parser.BaseParser
 
     async def _maybe_defer(self) -> None:
+        if self._deferred:
+            return
+
         if self._command is not None and (self._invoked or self._command).auto_defer:
             await self.app.rest.trigger_typing(self.channel_id)
             self._deferred = True

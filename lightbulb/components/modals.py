c1b486faa47fade9f5e809384b7bc5f0eee01c3e
nulldomain <git@nulldoma.in>
2025-08-20 23:45:02 +0100

    fix: remove unnecessary cast to str and actually add changelog fragment this time

--- lightbulb/components/modals.py
+++ lightbulb/components/modals.py
@@ -213,7 +213,7 @@ class Modal(
         """
         return self.add(
             TextInput(
-                custom_id=custom_id or f"lb_{uuid.uuid4()!s}",
+                custom_id=custom_id or f"lb_{uuid.uuid4()}",
                 style=hikari.TextInputStyle.SHORT,
                 label=label,
                 min_length=min_length,
@@ -254,7 +254,7 @@ class Modal(
         """
         return self.add(
             TextInput(
-                custom_id=custom_id or f"lb_{uuid.uuid4()!s}",
+                custom_id=custom_id or f"lb_{uuid.uuid4()}",
                 style=hikari.TextInputStyle.PARAGRAPH,
                 label=label,
                 min_length=min_length,
